dce4876f90e04d1736baca8cd5762e68f10955ab
AlphaMod1 <liutauras.vijeikis@gmail.com>
2019-12-08 21:15:52 +0200

    rockets now explode

--- js/rocket.js
+++ js/rocket.js
@@ -10,6 +10,9 @@ class Rocket {
         this.speed = 6;
         this.rotation = rotation || 0;
         this.firedBy = firedBy || 'enemy1';
+        this.explosionGifIndex = 0;
+        this.hasExploded = false;
+        this.spriteIterator = 4;
     }
     init() {
         this.generate()
@@ -35,12 +38,15 @@ class Rocket {
         this.rocketHTML.style.left = x + 'px';
         this.rocketHTML.style.transform = 'rotate(' + this.rotation + 'deg)';
     }
-    engine(){
+    engine() {
         this.fly();
         this.checkLifetime();
         window.requestAnimationFrame(() => { this.engine() });
     }
-    fly(){
+    fly() {
+        if (this.hasExploded) {
+            return;
+        }
         this.topSpeed = (Math.sin((this.rotation + 90) / 180 * Math.PI) * this.speed) * -1;
         this.leftSpeed = (Math.cos((this.rotation + 90) / 180 * Math.PI) * this.speed) * -1;
         let x = this.rocketHTML.style.left;
@@ -52,14 +58,72 @@ class Rocket {
         this.rocketHTML.style.top = y + 'px';
         this.rocketHTML.style.left = x + 'px';
     }
-    checkLifetime(){
+    checkLifetime() {
         this.currentLifeSpan++;
-        if(this.currentLifeSpan >= this.maxLifeSpan){
-            this.removeRocket();
+        if (this.currentLifeSpan >= this.maxLifeSpan) {
+            this.explodeRocket();
+            this.hasExploded = true;
         }
     }
-    removeRocket(){
-        this.rocketHTML.remove(); //exposion gif tba
+    explodeRocket() {
+
+        if (this.explosionGifIndex == 0) {
+            this.rocketHTML.classList.remove(`rocket-${this.firedBy}`);
+            this.rocketHTML.classList.remove('rocket');
+            this.rocketHTML.classList.add('expl');
+        }
+
+        switch (this.explosionGifIndex) {
+            case (this.spriteIterator * 0):
+                this.rocketHTML.classList.add('expl-1');
+                break;
+            case (this.spriteIterator * 1):
+                this.rocketHTML.classList.add('expl-2');
+                break;
+            case (this.spriteIterator * 2):
+                this.rocketHTML.classList.add('expl-3');
+                break;
+            case (this.spriteIterator * 3):
+                this.rocketHTML.classList.add('expl-4');
+                break;
+            case (this.spriteIterator * 4):
+                this.rocketHTML.classList.add('expl-5');
+                break;
+            case (this.spriteIterator * 5):
+                this.rocketHTML.classList.add('expl-6');
+                break;
+            case (this.spriteIterator * 6):
+                this.rocketHTML.classList.add('expl-7');
+                break;
+            case (this.spriteIterator * 7):
+                this.rocketHTML.classList.add('expl-8');
+                break;
+            case (this.spriteIterator * 8):
+                this.rocketHTML.classList.add('expl-9');
+                break;
+            case (this.spriteIterator * 9):
+                this.rocketHTML.classList.add('expl-10');
+                break;
+            case (this.spriteIterator * 10):
+                this.rocketHTML.classList.add('expl-11');
+                break;
+            case (this.spriteIterator * 11):
+                this.rocketHTML.classList.add('expl-12');
+                break;
+            case (this.spriteIterator * 12):
+                this.rocketHTML.classList.add('expl-13');
+                break;
+            case (this.spriteIterator * 13):
+                this.rocketHTML.classList.add('expl-14');
+                break;
+            case (this.spriteIterator * 14):
+                this.rocketHTML.classList.add('expl-15');
+                break;
+            case (this.spriteIterator * 16):
+                    this.rocketHTML.remove();
+                break;
+        }
+        this.explosionGifIndex++;
     }
 }
 
